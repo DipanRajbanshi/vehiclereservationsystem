d4ef23ae0f1ce1efd6387b24f68430433e29f225
DipanRajbanshi <dipanrajbanshi49@gmail.com>
2025-01-15 20:29:23 +0545

    Add files via upload

---
+++ html/homepage.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vehicle Reservation</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <style>
+        
+    </style>
+</head>
+
+<body>
+
+    <header>
+        <h1>VehiGO</h1>
+    </header>
+
+    <nav>
+        
+        <a href="#home">Home</a>
+        <a href="#aboutus">About</a>
+        <a href="#contact">Contact</a>
+      
+        
+        <div class="login_btn"> <a href="login.php" class="btn btn-xs uppercase" data-toggle="modal" data-dismiss="modal">Login / Register</a> </div>
+    </nav>
+
+    <div class="welcome-section">
+        <h2>Welcome to our Vehicle Reservation Service:<h1>VehiGO</h1></h2>
+        <p>Experience convenient and reliable vehicle reservations with us. Plan your trip with your loved ones and enjoy it.</p>
+    </div>
+
+    <div class="reservation-process">
+        <h2>Reservation Process</h2>
+        <p>1. Choose your desired vehicle</p>
+        <p>2. Select pickup and drop-off dates</p>
+        <p>3. Provide necessary details</p>
+        <p>4. Confirm and complete your reservation</p>
+        
+   
+    <footer>
+        <p>&copy; VehiGO</p>
+    </footer>
+
+</body>
+
+</html>
